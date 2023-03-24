9127e30351c6cf8c5959075fa80c425ef4e71b89
Hwirin-Kim <[kimhuirin@gmail.com]>
2023-03-24 11:01:29 +0900

    fix : 로그인 useEffect 간소화, 변수명 일부 변경

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -7,14 +7,14 @@ import classes from "./Register.module.css";
 import SubmitBtn from "./SubmitBtn";
 import Title from "./Title";
 export default function Register() {
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const [pwCheck, setPwCheck] = useState("");
-  const [pwCheckResult, setPwCheckResult] = useState("");
-  const [pwDuple, setPwDuple] = useState("");
-  const [idCheck, setIdCheck] = useState("");
-  const regId = /^[a-z0-9]*$/;
-  const regPw = /^[a-z0-9]{8,16}$/;
+  const [id, setId] = useState(""); // id input
+  const [pw, setPw] = useState(""); // pw input
+  const [pwCheck, setPwCheck] = useState(""); // pwCheck input
+  const [pwValidateResult, setPwValidateResult] = useState(""); // pwCheck result
+  const [pwCheckResult, setPwCheckResult] = useState(""); // pwDuple result
+  const [idDupleResult, setIdDupleResult] = useState(""); // idDuple result
+  const regId = /^[a-z0-9]*$/; // id 유효성
+  const regPw = /^[a-z0-9]{8,16}$/; // pw 유효성
 
   const navigator = useNavigate();
 
@@ -36,40 +36,46 @@ export default function Register() {
   //id 유효성검사 & 중복확인
   useEffect(() => {
     if (idCheckLoading) {
-      setIdCheck("Loading...");
-    } else if (id.length < 4 && regId.test(id)) {
-      setIdCheck("4글자 이상 입력하세요");
-    } else if (id.length > 3 && id.length < 17 && regId.test(id)) {
-      idCheckMutate({ username: id });
-    } else if (id.length > 16 && regId.test(id)) {
-      setIdCheck("16글자 이하로 입력하세요");
+      setIdDupleResult("Loading...");
     } else if (!regId.test(id)) {
-      setIdCheck("숫자와 영문만 입력하세요");
+      setIdDupleResult("숫자와 영문만 입력하세요");
+    } else if (id.length < 4) {
+      setIdDupleResult("4글자 이상 입력하세요");
+    } else if (id.length > 16) {
+      setIdDupleResult("16글자 이하로 입력하세요");
+    } else {
+      idCheckMutate({ username: id });
     }
   }, [id]);
 
+  //pw 유효성검사 & 중복확인
   useEffect(() => {
-    if (!regPw.test(pw)) {
-      setPwCheckResult("8~16글자의 영문과 숫자로 입력하세요");
-    } else if (regPw.test(pw)) {
-      setPwCheckResult("사용 가능한 비밀번호입니다.");
+    if (!pw) {
+      setPwValidateResult("비밀번호를 입력하세요");
+    } else if (!regPw.test(pw)) {
+      setPwValidateResult("8~16글자의 영문과 숫자로 입력하세요");
+    } else {
+      setPwValidateResult("사용 가능한 비밀번호입니다.");
     }
-    if (pw === pwCheck) {
-      setPwDuple("비밀번호가 일치합니다.");
-    } else if (pw !== pwCheck) {
-      setPwDuple("비밀번호가 다릅니다.");
+
+    if (!pwCheck) {
+      setPwCheckResult("비밀번호 확인을 입력하세요.");
+    } else if (pw === pwCheck) {
+      setPwCheckResult("비밀번호가 일치합니다.");
+    } else {
+      setPwCheckResult("비밀번호가 다릅니다.");
     }
   }, [pw, pwCheck]);
 
-  //중복 체크 mutation
+  //pw 중복 체크 mutation
   const { mutate: idCheckMutate, isLoading: idCheckLoading } = useMutation(
     (payload) => postIdCheck(payload),
     {
-      onSuccess: () => setIdCheck("사용 가능한 ID입니다."),
-      onError: () => setIdCheck("중복된 ID 입니다."),
+      onSuccess: () => setIdDupleResult("사용 가능한 ID입니다."),
+      onError: () => setIdDupleResult("중복된 ID 입니다."),
     }
   );
-
+  // Sign up mutation
   const { mutate: registerMutate } = useMutation(
     (payload) => postRegister(payload),
     {
@@ -81,6 +87,7 @@ export default function Register() {
     }
   );
 
+  // form onSubmit
   const onRegisterSubmit = (e) => {
     e.preventDefault();
     registerMutate({ username: id, password: pw, passwordConfirm: pwCheck });
@@ -94,21 +101,21 @@ export default function Register() {
           placeholder="ID"
           name="id"
           type="text"
-          text={idCheck}
+          text={idDupleResult}
         />
         <Input
           onChange={onPwChange}
           placeholder="PW"
           name="password"
           type="password"
-          text={pwCheckResult}
+          text={pwValidateResult}
         />
         <Input
           onChange={onPwCheckChange}
           placeholder="PW Check"
           name="password"
           type="password"
-          text={pwDuple}
+          text={pwCheckResult}
         />
         <SubmitBtn form="register" name="회원가입" />
       </form>
